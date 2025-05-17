8380347767d3605800ef45a7aca76ed93196ea78
MeexReay <meexreay@gmail.com>
2025-05-17 05:34:21 +0300

    give me kernel-trouble back

--- catch-kernel-trouble.sh
+++ catch-kernel-trouble.sh
@@ -10,7 +10,7 @@ fi
 
 pmbootstrap chroot adb pull /proc/last_kmsg /home/pmos
 
-if ! sudo mv $PMWORK/chroot_native/home/pmos/last_kmsg /tmp/full-kernel-trouble
+if ! sudo mv $PMWORK/chroot_native/home/pmos/last_kmsg /tmp/full-kernel-trouble.txt
 then
   echo "1. Enter to the recovery mode"
   echo "2. Run this script"
@@ -18,13 +18,13 @@ then
   exit 1
 fi
 
-tail -n $NUM_LINES /tmp/full-kernel-trouble > kernel-trouble
+tail -n $NUM_LINES /tmp/full-kernel-trouble > kernel-trouble.txt
 
 echo
 echo "Last $NUM_LINES log lines" 
 echo
 
-cat kernel-trouble
+cat kernel-trouble.txt
 
 echo
 echo "Saved to \"$PWD/kernel-trouble\""
